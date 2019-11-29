bff849b0a9e3266173719fb238b0565dffd2ab35
Erik Kasperson <chefkasperson@gmail.com>
2019-11-29 05:31:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,6 +17,11 @@ def reduce(array, x = nil)
   else
     total = array[0]
     i = 1
+  end
+  while i < array.length do 
+    total = yield(total, array[i])
+    i += 1 
+  end
+  total
 end
-while i < array.length
   
